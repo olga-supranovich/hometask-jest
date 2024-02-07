dd02daa0d1ebda243fa0e276c2eb86d6ea775bbd
olga-supranovich <olga.bogush87@gmail.com>
2024-02-07 12:33:47 +0100

    added tests for task 3.2.2

---
+++ task_3.2.2.test.js
@@ -0,0 +1,48 @@
+const [getMaxScoreStudent, getEarlierDate] = require("./task_3.2.2");
+
+const testArrays = [
+    [
+      [
+        { name: "Ivan", score: 40, date: "2022-10-11" },
+        { name: "Maria", score: 5, date: "2022-10-10" },
+        { name: "Olga", score: 0, date: "" },
+        { name: "Stepan", score: 35, date: "2022-10-12" },
+        { name: "Oleg", score: 9, date: "2022-10-01" },
+        { name: "Zanna", score: 15, date: "2022-10-11" },
+      ],
+      [{ name: 'Ivan', score: 40, date: '2022-10-11' }]
+    ],
+    [
+        [
+          { name: "Margo", score: 0, date: "2022-10-11" },
+          { name: "Natalia", score: 25, date: "2022-10-10" },
+          { name: "Marina", score: 25, date: "2022-10-01" },
+          { name: "Dmitry", score: 25, date: "2022-10-12" },
+          { name: "Denis", score: 0, date: "2022-10-02" },
+          { name: "Vadimyr", score: 1, date: "2022-10-11" },
+        ],
+        [{ name: "Marina", score: 25, date: "2022-10-01" }]
+      ],
+      [
+        [
+          { name: "Irina", score: 0, date: "2022-10-11" },
+          { name: "Vasily", score: 0, date: "2022-10-10" },
+          { name: "David", score: 0, date: "2022-10-11" },
+          { name: "Kristina", score: 0, date: "2022-10-12" },
+          { name: "Varvara", score: 0, date: "2022-10-01" },
+          { name: "Tanya", score: 0, date: "2022-10-11" },
+        ],
+        [{ name: "Varvara", score: 0, date: "2022-10-01" }]
+      ],
+  ];
+  
+  describe("Тестирование выборки студентов с макс баллами", () => {
+    it.each(testArrays)(
+      "Переменные значения %p полученные значения %p",
+      (students, result) => {
+        console.log("Input students:", students);
+        console.log("Expected result:", result);
+        expect(getMaxScoreStudent(students)).toMatchObject(result);
+      }
+    );
+  });
